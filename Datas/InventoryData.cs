3e9ab6b3ed0fa2f23d1be4fd76ae8680bf9767ae
Udi <dnel0605@gmail.com>
2025-02-06 20:22:05 +0900

    [fix] git fix
    파일 사라진 문제 수정

--- Datas/InventoryData.cs
+++ Datas/InventoryData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,49 @@ namespace Team34_TextRPG
 {
 	public class InventoryData
 	{
+		HashSet<string> myItems = new HashSet<string>();
+		Item? weapon;
+		Item? armor;
+		 
+		public bool isItemInInventory(Item item)
+		{
+			return myItems.Contains(item.name);
+		}
+
+
+		public List<Item> GetMyItems()
+		{
+			List<Item> list = new List<Item>();
+
+			// TODO
+			// Dictionary로 저장된 아이템 목록에 접속 후 리스트에 담기
+			//foreach (var item in myItems)
+		//		list.Add()
+		 
+			return list;
+		}
+		public bool IsEquipped(Item item)
+		{
+			return weapon == item || armor == item;
+		}
+
+		public void EquipItem(Item item)
+		{ 
+			if (item is Weapon)
+			{
+				if (weapon == item)
+					weapon = null;
+				else
+					weapon = item;
+			}
+			else
+			{
+				if (armor == item)
+					armor = null;
+				else
+					armor = item; 
+			}
+		}
 
 	}
 }
