524e4c05916f379c677235e498228955385034e2
rlck339 <a95895899@gmail.com>
2025-02-06 17:09:15 +0900

    [feat]town_1

--- Scene/Town.cs
+++ Scene/Town.cs
@@ -12,10 +12,24 @@ namespace Team34_TextRPG
 
 	public class Town
 	{
+	
 
 		public void EnterTown()
 		{
+			Console.Clear();
+            Console.WriteLine("이곳은 마을 입니다.");
+            Console.WriteLine("아래의 기능중 하나를 선택해 주세요.");
+			Console.WriteLine();
+			Console.WriteLine("1. 상태창");
+			Console.WriteLine("2. 전투시작");
+			Console.WriteLine();
+			Console.WriteLine("0. 나가기");
 
+			int value = SpartaRPG.SelectOption(0,2);
+			
+			
+				
+				
 		}
 	}
 }
