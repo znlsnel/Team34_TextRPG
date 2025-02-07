0726a8e0df92e031dc723aaf03599224c94ba02b
Udi <dnel0605@gmail.com>
2025-02-07 14:07:01 +0900

    [feat] Monster Data
    스테이지별 몬스터를 관리하는 class, 몬스터 class 추가

---
+++ Datas/MonsterData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team34_TextRPG
+{
+	public class MonsterData
+	{
+		public List<Monster[]> stages = new List<Monster[]>();
+		Random rand = new Random();
+		public MonsterData() 
+		{
+			InitMonster();		
+		}
+
+		void InitMonster()
+		{
+			// Stage 1 
+			stages.Add(new Monster[4]
+			{
+				new Monster("미니언", 2, 5, 15),
+				new Monster("공허충", 3, 7, 20),
+				new Monster("대포미니언", 4, 10, 25),
+				new Monster("슈퍼미니언", 5, 15, 30),
+			});
+
+			// Stage 2 
+			stages.Add(new Monster[4]
+			{
+				new Monster("슬라임", 5, 12, 40),
+				new Monster("고블린", 6, 15, 40),
+				new Monster("오크", 7, 20, 50),
+				new Monster("트롤 ", 8, 25, 70),
+			});
+
+			// Stage 3 
+			stages.Add(new Monster[4]
+			{
+				new Monster("스켈레톤", 8, 35, 50),
+				new Monster("리자드맨 ", 9, 30, 70),
+				new Monster("미믹  ", 10, 25, 150),
+				new Monster("하피 ", 11, 55, 90),
+			});
+		}
+
+		public List<Monster> GetMonster(int idx)
+		{
+			List<Monster> list = new List<Monster>();
+
+			int length = stages[idx].Length;
+			int cnt = rand.Next(3, 6);
+			while (cnt-- > 0)  
+				list.Add(stages[idx][rand.Next(0, length)]);
+
+			return list;
+		}
+	}
+}
